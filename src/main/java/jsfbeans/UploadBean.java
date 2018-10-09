1802e50238f7f298350bd0972ff856f850a89ba7
burhil <burhil@NB037.LZN.local>
2018-10-09 11:24:51 +0200

    View and ManagedBean for upload

--- src/main/java/jsfbeans/UploadBean.java
+++ src/main/java/jsfbeans/UploadBean.java
@@ -1,16 +1,28 @@
 package jsfbeans;
 
+
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.ValidatorException;
 import javax.servlet.http.Part;
 
+@ManagedBean
+@SessionScoped
 public class UploadBean {
 	
 	private Part file;
 	@SuppressWarnings("unused")
 	private String fileContent;
 
+	@SuppressWarnings("resource")
 	public void upload() {
 		try {
 			fileContent = new Scanner(file.getInputStream()).useDelimiter("\\A").next();
@@ -25,5 +37,19 @@ public class UploadBean {
 
 	public void setFile(Part file) {
 		this.file = file;
+	}
+	
+	public void validateFile(FacesContext ctx, UIComponent comp, Object value) {
+		List<FacesMessage> msgs = new ArrayList<FacesMessage>();
+		Part file = (Part)value;
+		if (file.getSize() > 1024) {
+			msgs.add(new FacesMessage("file too big"));
+		}
+		if (!"text/plain".equals(file.getContentType())) {
+			msgs.add(new FacesMessage("not a text file"));
+		}
+		if (!msgs.isEmpty()) {
+			throw new ValidatorException(msgs);
+		}
 	}
 }
